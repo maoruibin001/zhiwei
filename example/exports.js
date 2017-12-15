9ce61455d4dd3f22ed4626839ed981c67186d595
maoruibin001 <www.maoruibin@163.com>
2017-12-15 15:22:32 +0800

    上传网站logo，感谢@李晓君订单dddsafdsaf，是这个美丽并且无私的妹子

---
+++ example/exports.js
@@ -0,0 +1,6 @@
+/**
+ * Created by lenovo on 2017/12/12.
+ */
+export default {
+  name: 'mao'
+}
---
+++ example/import.js
@@ -0,0 +1,6 @@
+/**
+ * Created by lenovo on 2017/12/12.
+ */
+import {name} from './exports';
+
+console.log(name);
